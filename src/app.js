49a4a4b79df263001754c2b606c35d9559ccf8ff
Vicki Long <vickilong@users.noreply.github.com>
2015-02-14 16:52:42 -0500

    scrollable cards

--- src/app.js
+++ src/app.js
@@ -5,54 +5,53 @@
  */
 
 var UI = require('ui');
-var Vector2 = require('vector2');
+var Vibe = require('ui/vibe');
 
 var main = new UI.Card({
   title: 'Pebble.js',
   icon: 'images/menu_icon.png',
   subtitle: 'Hello World!',
-  body: 'Press any button.'
+  body: 'Press any button. blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah',
+  scrollable: true
 });
 
 main.show();
 
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
-});
+// main.on('click', 'up', function(e) {
+//   var menu = new UI.Menu({
+//     sections: [{
+//       items: [{
+//         title: 'Pebble.js',
+//         icon: 'images/menu_icon.png',
+//         subtitle: 'Can do Menus'
+//       }, {
+//         title: 'Second Item',
+//         subtitle: 'Subtitle Text'
+//       }]
+//     }]
+//   });
+//   menu.on('select', function(e) {
+//     console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
+//     console.log('The item is titled "' + e.item.title + '"');
+//   });
+//   menu.show();
+//   Vibe.vibrate('short');
+// });
 
 main.on('click', 'select', function(e) {
-  var wind = new UI.Window();
-  var textfield = new UI.Text({
-    position: new Vector2(0, 50),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
+  var wind = new UI.Card({
+    body: 'blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah',
+    scrollable: true
   });
-  wind.add(textfield);
   wind.show();
+  Vibe.vibrate('long');
 });
 
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
-});
+// main.on('click', 'down', function(e) {
+//   var card = new UI.Card();
+//   card.title('A Card');
+//   card.subtitle('Is a Window');
+//   card.body('The simplest window type in Pebble.js.');
+//   card.show();
+//   Vibe.vibrate('double');
+// });
